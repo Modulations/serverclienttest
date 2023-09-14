18bd8fafabef89202fd5ad74298ecd1baaddcd3a
Luke DaSilva <lad5@njit.edu>
2023-09-14 17:36:29 -0400

    Redis testing

--- test.js
+++ test.js
@@ -1,17 +1,43 @@
-var focus = {}
+// var focus = {}
 
-// setup workers here
-var workerFarm = require('worker-farm');
-var workers = workerFarm(require.resolve('./child.js'));
+// // setup workers here
+// var workerFarm = require('worker-farm');
+// var workers = workerFarm(require.resolve('./child.js'));
 
-var ret = 0;
+// var ret = 0;
  
-for (var i = 0; i < 10; i++) {
-  workers('#' + i + ' FOO', focus, function (err, outp) {
-    console.log(outp)
-    if (++ret == 10) { workerFarm.end(workers) }
-    console.log(focus)
+// for (var i = 0; i < 10; i++) {
+//   workers('#' + i + ' FOO', focus, function (err, outp) {
+//     console.log(outp)
+//     if (++ret == 10) { workerFarm.end(workers) }
+//     console.log(focus)
+//     })
+// }
+
+// console.log("Initial Commit")
+
+var redis = require("redis")
+var config = {
+    url: 'redis://localhost:6379'
+}
+const client = redis.createClient(config);
+
+yerr = async () => {
+    client.on('error', (err) => {
+        console.log(err)
     })
+
+    await client.connect();
+    // await here?
+    await client.hSet('user-session:123', {
+        name: 'John',
+        surname: 'Smith',
+        company: 'Redis',
+        age: 29
+    })
+
+    let userSession = await client.hGetAll('user-session:123');
+    console.log(JSON.stringify(userSession, null, 2));
 }
 
-console.log("Initial Commit")
+yerr()
